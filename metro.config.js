a020a812768dbae7c6f78bb586f29b6268ec3d23
nasway2 <as9876958024as@gmail.com>
2026-04-11 01:35:25 +0300

    Fix metro.config.js for GitHub Actions

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Эти настройки критически важны для Firebase Auth
-config.resolver.unstable_enablePackageExports = false;
+// Убираем использование toReversed()
+// Просто добавляем нужные расширения
 config.resolver.sourceExts.push('cjs');
 
 module.exports = config;
