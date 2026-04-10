fc96298e90dae0697694df68f496c76fae6210dd
nasway2 <as9876958024as@gmail.com>
2026-04-11 01:48:21 +0300

    Simplify metro.config.js for older Node.js

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Убираем использование toReversed()
-// Просто добавляем нужные расширения
+// Просто добавляем поддержку .cjs файлов
 config.resolver.sourceExts.push('cjs');
 
 module.exports = config;
