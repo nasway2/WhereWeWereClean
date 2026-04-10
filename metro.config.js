2b1d080678e1345504121930099d82da8a88d10b
nasway2 <as9876958024as@gmail.com>
2026-04-11 01:58:53 +0300

    Remove metro.config.js

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Просто добавляем поддержку .cjs файлов
-config.resolver.sourceExts.push('cjs');
-
-module.exports = config;
